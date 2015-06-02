46b4976efe122eaabb0914a91ee04ea0e5546366
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2015-06-02 10:43:10 +0200

    rearranged the ordering of input parameters in the sources, should not have any effect besides being more readable in the code.

--- src/RidgeRaceInput.cpp
+++ src/RidgeRaceInput.cpp
@@ -20,39 +20,32 @@ RidgeRaceInput readConfigFile(string path) {
 
 	// input data
 	rri.inTree = (string) cf.Value("input", "inTree");
+	rri.startTreeSize = cf.Value("input", "startTreeSize");
+	rri.stopTreeSize = cf.Value("input", "stopTreeSize");
+	rri.stepTreeSize = cf.Value("input", "stepTreeSize");
+	rri.repeatTreeSim = cf.Value("input", "repeatTreeSim");
+	rri.treeSimFile = (string) cf.Value("input", "treeSimFile");
 
 	// simulation parameters
 	rri.grandMean = cf.Value("simulation", "grandMean");
 	rri.maxNrOfRegimes = cf.Value("simulation", "maxNrOfRegimes");
 	rri.nrOfRegimesStepSize = cf.Value("simulation", "nrOfRegimesStepSize");
-
+	rri.stdMode = (string) cf.Value("simulation", "stdMode");
 	rri.startStd = cf.Value("simulation", "startStd");
 	rri.endStd = cf.Value("simulation", "endStd");
 	rri.stepStd = cf.Value("simulation", "stepStd");
 
-	rri.stdMode = (string) cf.Value("simulation", "stdMode");
-
 	// evaluation parameters
-	rri.roundsOfEvolution = cf.Value("evaluation", "roundsOfEvolution");
 	rri.roundsOfSimulation = cf.Value("evaluation", "roundsOfSimulation");
-	rri.testCorrelationOutputFile = (string) cf.Value("evaluation",
-			"testCorrelationOutputFile");
-
+	rri.roundsOfEvolution = cf.Value("evaluation", "roundsOfEvolution");
+	rri.testCorrelationOutputFile = (string) cf.Value("evaluation", "testCorrelationOutputFile");
 	rri.drawTrees = cf.Value("evaluation", "drawTrees");
 
-	rri.startTreeSize = cf.Value("input", "startTreeSize");
-	rri.stopTreeSize = cf.Value("input", "stopTreeSize");
-	rri.stepTreeSize = cf.Value("input", "stepTreeSize");
-	rri.repeatTreeSim = cf.Value("input", "repeatTreeSim");
-
-	rri.treeSimFile = (string) cf.Value("input", "treeSimFile");
-
+	// inference
 	rri.phenoPath = (string) cf.Value("inference", "phenoPath");
 	rri.sequencePath = (string) cf.Value("inference", "sequencePath");
 	rri.distanceMatrixPath = (string) cf.Value("inference", "distanceMatrixPath");
 	rri.posNamePath = (string) cf.Value("inference", "posNamePath");
-
-
 
 	return rri;
 }
