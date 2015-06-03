0a2a781eaa2386c411cb00809edb9c4b3abfafdf
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2015-06-03 08:28:16 +0200

    forget to commit this file in the last commit.

---
+++ include/prefix.h
@@ -0,0 +1,9 @@
+#ifndef PREFIX_HH
+#define PREFIX_HH
+
+namespace RidgeRace {
+	extern const char progname[]; //name of the binary / program
+	extern const char prefix[]; //the directory in which the program will be installed. Important to know, because e.g. R script are positioned relative to this file in ../share/RidgeRace/
+}
+
+#endif
